971750dc1d219afa3fc298e5cfdff19d73e29823
Michel França <michel.assis@targetsoftware.com.br>
2024-07-29 12:30:50 -0300

    test com requisição ssr

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+const data = await $fetch("https://pokeapi.co/api/v2/pokemon");
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    {{ data }}
   </div>
 </template>
